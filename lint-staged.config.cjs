50caec812f791337d22458286cd55d9c69a450cf
waf2020 <549426210@qq.com>
2024-03-04 17:59:40 +0800

    sss

---
+++ lint-staged.config.cjs
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  'package.json': ['prettier --write'],
+  '*.vue': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
+  '*.{scss,less,styl,html}': ['stylelint --fix', 'prettier --write'],
+  '*.md': ['prettier --write']
+}
